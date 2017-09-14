771f0f63c76eb4ed104a43e211a53b41b3046dc7
Hack Reactor Student <student@hackreactor.com>
2017-09-14 14:09:04 -0700

    Handle both text and chatterbox request body

--- server/request-handler.js
+++ server/request-handler.js
@@ -17,55 +17,57 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-var fs = require('fs');
+// var fs = require('fs');
 var path = require('path');
 var messages = [];
 var requestHandler = function(request, response) {
 
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+// set default headers
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  // console.log(request);
-  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // console.log(request.body);
-
-
-
-  // The outgoing status.
   var statusCode = 200;
 
-  // if request type is POST
-    // get, parse request body
-    // add to file fs.appendFile
-  if (request.method === 'POST' && (request.url === '/classes/messages' || request.url === '/classes/room')) {
+  if (request.url !== '/classes/messages' && request.url !== '/classes/room') {
+    console.log('404 current request: ', request.url, request.method);
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify({'results': ['hello', 'world']}));
+  }
+
+  // handle post requests
+  if (request.method === 'POST') {
     request.on('data', function(chunk) { // on receiving data
       // var data = [];
       // data.push(chunk);
 
-      // convert data to object
+      // convert TEST data to object
       var body = chunk.toString();
-      // var message = {};
-      // var processMessage = body.split('&');
-      // for (var i = 0; i < processMessage.length; i++) {
-      //   var array = processMessage[i].split('=');
-      //   message[array[0]] = array[1];
-      // }
-      //console.log(body);
-      var message = JSON.parse(body);
+      if (body.indexOf('=') === -1) {
+        console.log('this is a test');
+        var message = JSON.parse(body);
+
+      // process CHATTERBOX data
+      } else {
+        var message = {};
+        body = body.replace('+', ' ');
+        var processMessage = body.split('&');
+        console.log('Split message', processMessage);
+
+        for (var i = 0; i < processMessage.length; i++) {
+          var array = processMessage[i].split('=');
+          message[array[0]] = array[1];
+          console.log('key', array[0]);
+          console.log('value', array[1]);
+        }
+      }
 
       messages.push(message);
-      console.log(messages);
+
+
+      //console.log(body);
+
+      // console.log(messages);
       // var stringifiedMessageArray = '';
 
       // retrieve current messages in file
@@ -126,62 +128,18 @@ var requestHandler = function(request, response) {
     });
 
     statusCode = 201;
-    var headers = defaultCorsHeaders;
-    headers['Content-Type'] = 'application/json';
     response.writeHead(statusCode, headers);
-    response.end(JSON.stringify({'results': ['messages', 'posted']}));
+    response.end();
 
-  } else if(request.method === 'GET' && (request.url === '/classes/messages' || request.url === '/classes/rooms')){
-    var headers = defaultCorsHeaders;
-    headers['Content-Type'] = 'application/json';
+  // handle get requests
+  } else if(request.method === 'GET') {
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify({'results': messages}));
-  } else {
-    console.log('404 current request: ', request.url);
-    console.log('404 current request: ', request.method);
-    statusCode = 404;
-    var headers = defaultCorsHeaders;
-    headers['Content-Type'] = 'application/json';
+
+  } else if (request.method === 'OPTIONS') {
     response.writeHead(statusCode, headers);
-    response.end(JSON.stringify({'results': ['hello', 'world']}));
+    response.end('OK');
   }
 };
-
-
-  //else if(request.method === 'POST' && request.url === '/classes/room'){ //end of POST ********************************************************************
-
-  // See the note below about CORS headers.
-
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  //   var headers = defaultCorsHeaders;
-  // headers['Content-Type'] = 'application/json';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-
 
 module.exports.requestHandler = requestHandler;
