5142d2dd4e3f56b4c2266172f91d4911820a937a
Hack Reactor Student <student@hackreactor.com>
2017-09-13 18:44:33 -0700

    receives POST requests but doesn't handle it

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,5 +1,6 @@
 /* Import node's http module: */
 var http = require('http');
+var handleRequest = require('./request-handler.js');
 
 
 // Every server needs to listen on a port with a unique number. The
@@ -25,9 +26,7 @@ var ip = '127.0.0.1';
 var server = http.createServer(handleRequest);
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
-
 // To start this server, run:
-//
 //   node basic-server.js
 //
 // on the command line.
@@ -38,4 +37,3 @@ server.listen(port, ip);
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
-
--- server/request-handler.js
+++ server/request-handler.js
@@ -11,7 +11,14 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+var fs = require('fs');
+var messages = [];
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -27,10 +34,46 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  // console.log(request);
+  console.log('Serving request type ' + request.method + ' for url ' + request.url + 'with whole request: ');
+  console.log(request.body);
+
+
 
   // The outgoing status.
   var statusCode = 200;
+
+  // if request type is POST
+    // get, parse request body
+    // add to file fs.appendFile
+  if (request.method === 'POST') {
+    request.on('data', function(chunk){
+      var body = chunk.toString();
+      console.log('chunk is type: ', chunk);
+      var message = {};
+
+      // username=hodor&text=hello&roomname=lobby
+      var processMessage = body.split('&');
+
+
+
+
+
+      fs.appendFile('messages.json', body, function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('Appended message!');
+      });
+    // });
+    }).on('end', () => {
+    //   body = Buffer.concat(body).toString();
+    //   console.log('body is: ' + body);
+    });
+
+
+    statusCode = 201;
+  }
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -39,7 +82,7 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -52,7 +95,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end(JSON.stringify({'results': ['hello', 'world']}));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +107,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
+
+module.exports = requestHandler;
